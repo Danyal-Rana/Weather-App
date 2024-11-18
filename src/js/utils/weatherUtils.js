1ca1d13c893eac1816a449e1450ee2f2f48972e7
Danyal-Rana <ranadanyalarshad@gmail.com>
2024-11-18 08:57:49 +0500

    Remove unused CSS files and update background image URLs in weatherUtils.js

--- src/js/utils/weatherUtils.js
+++ src/js/utils/weatherUtils.js
@@ -5,25 +5,34 @@ export function getWindDirection(degree) {
 
 export function updateBackgroundImage(weatherCondition) {
     let imageUrl;
-    switch(weatherCondition.toLowerCase()) {
+    switch (weatherCondition.toLowerCase()) {
         case 'clear':
-            imageUrl = 'https://example.com/sunny-background.jpg';
+            imageUrl = 'https://images.unsplash.com/photo-1571878091425-0b948ed28d55?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your sunny background image URL
+            break;
+        case 'smoke':
+            imageUrl = 'https://images.unsplash.com/photo-1704590973061-258a406941e1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your sunny background image URL
             break;
         case 'clouds':
-            imageUrl = 'https://example.com/cloudy-background.jpg';
+            imageUrl = 'https://images.unsplash.com/photo-1527377844612-ae9febb890c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your cloudy background image URL
             break;
         case 'rain':
         case 'drizzle':
-            imageUrl = 'https://example.com/rainy-background.jpg';
+            imageUrl = 'https://images.unsplash.com/photo-1545622757-7b91055c618b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your rainy background image URL
             break;
         case 'thunderstorm':
-            imageUrl = 'https://example.com/stormy-background.jpg';
+            imageUrl = 'https://images.unsplash.com/photo-1561485132-59468cd0b553?q=80&w=1952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your stormy background image URL
             break;
         case 'snow':
-            imageUrl = 'https://example.com/snowy-background.jpg';
+            imageUrl = 'https://images.unsplash.com/photo-1478719059408-592965723cbc?q=80&w=2026&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your snowy background image URL
+            break;
+        case 'haze':
+            imageUrl = 'https://images.unsplash.com/photo-1436459826008-8fd497f03742?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your snowy background image URL
+            break;
+        case 'fog':
+            imageUrl = 'https://images.unsplash.com/photo-1642947945591-ce9153f8a5a7?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Replace with your snowy background image URL
             break;
         default:
-            imageUrl = 'https://example.com/default-background.jpg';
+            imageUrl = 'https://images.unsplash.com/photo-1698780717462-a757e4990f78?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
     }
     document.body.style.backgroundImage = `url('${imageUrl}')`;
 }
